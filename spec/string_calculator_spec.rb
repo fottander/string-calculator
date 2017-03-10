d23903bc99a085f103a33f0627f0c8afc19bc0d2
jocontreras <jen.almarasy@gmail.com>
2017-03-10 10:09:35 +0100

    add test for returning 1

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,5 @@
 # Create a simple String calculator with a method int Add(string numbers)
-# The method can take 0, 1 or 2 numbers, and will return their sum 
+# The method can take 0, 1 or 2 numbers, and will return their sum
 # (for an empty string it will return 0) for example “” or “1” or “1,2”
 
 # Start with the simplest test case of an empty string and move to 1 and two numbers
@@ -11,5 +11,9 @@ require './lib/string_calculator'
 describe StringCalculator do
   it 'returns 0 for an empty string' do
     expect(subject.add('')).to eq 0
+  end
+
+  it 'returns 1 when receives 1' do
+    expect(subject.add('1')).to eq 1
   end
 end
